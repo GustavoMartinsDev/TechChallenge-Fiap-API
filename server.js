8a700ba670c8b91698b02eb83dc973a501bfb342
GustavoMartinsDev <gustavomartins.developer@gmail.com>
2025-01-22 22:46:35 -0300

    atualização porta local

--- server.js
+++ server.js
@@ -253,7 +253,7 @@ db.once("open", async function () {
     }
   });
 
-  const PORT = process.env.PORT || 4000;
+  const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
